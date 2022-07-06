1771bd186c9ebae229cf050f8e01c741ce3c12f0
PavelB1349 <108696097+PavelB1349@users.noreply.github.com>
2022-07-07 02:02:31 +0600

    Add files via upload

---
+++ exercise/Program.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*  Создать приложение для работы магазина продающего
+ *  музыкальные альбомы музыкальных групп и исполнителей
+ *  В приложении должны быть реализованы CRUD - операции:
+ *  CREATE, READ, UPDATE, DELETE.
+ *  Т.е. пользователь приложения может создать запись
+ *  содержащую информацию о новом альбоме, изменить альбом,
+ *  удалить альбом, вывести информацию об альбоме.
+ *
+ *  Также в приложении необходимо реализовать поиск:
+ *  - Всех альбомов и их исполнителей по имени исполнителя
+ *  (музыкальной группы)
+ *  - Вывод только информации об исполнителях
+ *  имеющихся в базе (Имя Фамилия (либо название группы)),
+ *  Год рождения (либо дата основания группы), Биография
+ *  (история существования группы)
+ *  - Вывод информации только о музыкальных альбомах
+ *  (Исполнитель (Группа), Название альбома, год выпуска,
+ *  стоимость, есть альбом в наличии или нет)
+ *  - По дате выпуска музыкального альбома
+ *  - По цене
+ *  - По названию музыкального альбома
+ *  - Вывод всех альбомов отсортированных по имени исполнителя
+ *  - Отсортированных по названию альбома
+ *  - Отсортированных по дате выпуска
+ *
+ *  В приложении должен присутствовать класс РЕПОЗИТОРИЙ,
+ *  имитирующий базу данных в которой хранятся альбомы.
+ *  Репозиторий заполнить минимум 10-ю музыкальными альбомами.
+ *  Создать удобное меню для работы, а также реализовать работу
+ *  приложения через аргументы командной строки.
+ */
+namespace exercise
+{
+    interface IAlbumInfo
+    {
+        void GetAlbumInfo();
+        void GetArtistInfo();
+    }
+    interface IAlbum : IAlbumInfo
+    {
+        string Name { get; }
+        string Title { get; }
+        DateTime ReleaseYear { get; }
+        int Price { get; }
+        bool Availability { get; }
+    }
+     class Artist : IAlbum, IAlbumInfo
+    {
+        public string Name { get; set; }
+        public DateTime YearOfBirth { get; set; }
+        public string Biography { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseYear { get; set; }
+        public int Price { get; set; }
+        public bool Availability { get; set;}
+        
+        public void GetArtistInfo()
+        {
+            Console.WriteLine($"Имя артиста {Name}\nДата рождения: {YearOfBirth}\nБиография: {Biography}\n");
+        }
+        public void GetAlbumInfo()
+        {
+            Console.WriteLine($"Название альбома: {Title}\nДата релиза: {ReleaseYear}\nЦена: {Price}\nНаличие: {Availability}\n");
+        }
+
+    }
+    public class Group : IAlbum, IAlbumInfo
+    {
+        public string Name { get; set; }
+        public DateTime FoundingDate { get; set; }
+        public string HistoryOfGroup { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseYear { get; set; }
+        public int Price { get; set; }
+        public bool Availability { get; set; }
+        public void GetArtistInfo()
+        {
+            Console.WriteLine($"Группа: {Name}\nДата основания: {FoundingDate}\nКраткая история группы:\n{HistoryOfGroup}\n");
+        }
+        public void GetAlbumInfo()
+        {
+            Console.WriteLine($"Название альбома: {Title}\nДата релиза: {ReleaseYear}\nЦена: {Price}\nНаличие: {Availability}\n");
+        }
+    }
+
+    
+    internal class Program
+    {
+
+        public static void CreateArtistAlbum()
+        {
+            Console.WriteLine("Введите имя артиста");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения");
+            DateTime birthDay = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите название альбома");
+            string nameAlbum = Console.ReadLine();
+            Console.WriteLine("Введите дату релиза");
+            DateTime release = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите цену");
+            int price = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите наличие (true\false)");
+            bool availability = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Введите биографию артиста");
+            string biography = Console.ReadLine();           
+
+            Repository.albums.Add( new Artist() {Name = name, YearOfBirth = birthDay, Title = nameAlbum,
+                ReleaseYear = release, Price = price, Availability = availability, Biography = biography});
+        }
+        public static void CreateGroupAlbum()
+        {
+            Console.WriteLine("Введите название группы");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите дату основания");
+            DateTime foundationDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите название альбома");
+            string nameAlbum = Console.ReadLine();
+            Console.WriteLine("Введите дату релиза");
+            DateTime release = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите цену");
+            int price = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите наличие (true\false)");
+            bool availability = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Введите краткую историю группы");
+            string history = Console.ReadLine();
+
+            Repository.albums.Add(new Group()
+            {
+                Name = name,
+                FoundingDate = foundationDate,
+                Title = nameAlbum,
+                ReleaseYear = release,
+                Price = price,
+                Availability = availability,
+                HistoryOfGroup = history
+            });
+        }
+        /// <summary>
+        /// Всех альбомов и их исполнителей по имени исполнителя (музыкальной группы)
+        /// </summary>
+        public static void PrintAlbumFromArtistName()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            var albums = Repository.albums;
+
+            foreach (var item in albums)
+            {
+                item.GetAlbumInfo();
+                Console.WriteLine();
+            }          
+
+
+            int userInput;
+            do
+            {
+                Console.WriteLine("1 - Создать новый альбом артиста\n2 - Создать новый альбом группы\n3 - Поиск альбома по исполнителю\n4 - Поиск информации по исполнителю\n" +
+                    "5 - Поиск информации об альбоме по исполнителю\n6 - Поиск по цене\n7 - Поиск по названию альбома\n8 - Поиск по дате релиза\n" +
+                    "9 - Вывод всех альбомов отсортированных по имени исполнителя\n10 - Вывод всех альбомов отсортированных по названию альбома\n" +
+                    "11 - Вывод всех альбомов отсортированных по дате релиза\n12 - Вывод всех альбомов\n0 - Выход");
+                Console.WriteLine("Выберите действие");
+                userInput = int.Parse(Console.ReadLine());
+                string name = "";                
+                switch (userInput)
+                {
+                    
+                    case 1:
+                        CreateArtistAlbum();
+                        Console.WriteLine("Альбом добавлен.\n");
+                        break;
+                    case 2:
+                        CreateGroupAlbum();
+                        Console.WriteLine("Альбом добавлен.\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите имя исполнителя или название группы");
+                        name = Console.ReadLine();
+                        var result3 = Repository.albums.Where(x => x.Name.Contains(name));
+                        Console.WriteLine("Информация по альбомам:\n");
+                        foreach (var album in result3)
+                        {
+                            album.GetArtistInfo();
+                            album.GetAlbumInfo();
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Введите имя исполнителя или название группы");
+                        name = Console.ReadLine();
+                        var result4 = Repository.albums.Where(x => x.Name.Contains(name));
+                        foreach (var album in result4)
+                        {
+                            album.GetArtistInfo();
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Введите имя исполнителя или название группы");
+                        name = Console.ReadLine();
+                        var result5 = Repository.albums.Where(x => x.Name.Contains(name));
+                        foreach (var album in result5)
+                        {
+                            album.GetAlbumInfo();
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine("Введите цену альбома");
+                        int userPrice = int.Parse(Console.ReadLine());
+                        var result6 = Repository.albums.Where(x => x.Price == userPrice);
+                        foreach (var album in result6)
+                        {
+                            album.GetAlbumInfo();
+                            album.GetArtistInfo();
+                        }
+                        break;
+                    case 7:
+                        Console.WriteLine("Введите название альбома");
+                        name = Console.ReadLine();
+                        var result7 = Repository.albums.Where(x => x.Title.Contains(name));
+                        foreach (var album in result7)
+                        {
+                            album.GetAlbumInfo();
+                            album.GetArtistInfo();
+                        }
+                        break;
+                    case 8:
+                        Console.WriteLine("Введите дату релиза альбома");
+                        DateTime dateTime = DateTime.Parse(Console.ReadLine());
+                        var result8 = Repository.albums.Where(x => x.ReleaseYear == dateTime);
+                        foreach (var album in result8)
+                        {
+                            album?.GetAlbumInfo();
+                            album?.GetArtistInfo();
+                        }
+                        break;
+                    case 9:
+                        Console.WriteLine("Все альбомы отсортированные по имени исполнителя");
+                        var result9 = Repository.albums.OrderBy(x => x.Name);
+                        foreach (var album in result9)
+                        {
+                            album.GetArtistInfo();
+                            album.GetAlbumInfo();
+                        }
+                        break;
+                    case 10:
+                        Console.WriteLine("все альбомы отсортированные по названию альбома");
+                        var result10 = Repository.albums.OrderBy(x => x.Title);
+                        foreach (var album in result10)
+                        {
+                            album.GetAlbumInfo();
+                            album.GetArtistInfo();
+                        }
+                        break;
+                    case 11:
+                        Console.WriteLine("Все альбомы отсортированные по дате релиза");
+                        var result11 = Repository.albums.OrderBy(x => x.ReleaseYear);
+                        foreach (var album in result11)
+                        {
+                            album.GetArtistInfo();
+                            album.GetAlbumInfo();
+                        }
+                        break;
+                    case 12:
+                        Console.WriteLine("Все альбомы");
+                        foreach (var album in albums)
+                        {
+                            album.GetAlbumInfo();
+                            album.GetArtistInfo();
+                        }
+                        break;
+                    default:
+                        if (userInput == 0)
+                        {
+                            Console.WriteLine("Завершение работы.\nВсего доброго!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода команды.");
+                        }
+                        break;
+                }
+            } while (userInput != 0) ;
+        Console.ReadLine();
+        }
+    }
+}
---
+++ exercise/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    internal class Repository
+    {
+        public static List<IAlbum> albums  = new List<IAlbum>()
+        {
+           new Group {Name ="Пикник", FoundingDate = new DateTime(1978,02,01), Price= 1500, Title = "Говорит и показывает", ReleaseYear = new DateTime(2003,06,15), Availability = true, HistoryOfGroup= "советская и российская рок-группа, основанная в Ленинграде в 1978 году."},
+           new Group {Name ="Linkin Park", FoundingDate= new DateTime(2000, 05,04), HistoryOfGroup= "американская рок-группа, основанная в 1996 году под названием Xero и исполняющая музыку преимущественно в стилях альтернативный метал, ню-метал и рэп-метал (раннее творчество)," +
+               " альтернативный рок, электроник-рок, поп и поп-рок (позднее творчество). " +
+               "Существуя с 2000 года под названием Linkin Park, группа была шесть раз номинирована и два раза удостоилась награды «Грэмми».", Price = 1800, Title = "Meteora", ReleaseYear = new DateTime(2003,03,25), Availability = true},
+           new Group {Name = "Nirvana", FoundingDate = new DateTime(1987,03,03), HistoryOfGroup = " американская рок-группа, созданная вокалистом и гитаристом Куртом Кобейном и басистом Кристом Новоселичем в Абердине, штат Вашингтон, в 1987 году.",
+                    Price = 1600, Title = "Nevermind", Availability = true, ReleaseYear = new DateTime(1991,09,24)},
+           new Group {Name = "Агата Кристи", FoundingDate = new DateTime(1987,05,13), HistoryOfGroup= "«Ага́та Кри́сти» — советская и российская рок-группа, одна из наиболее популярных в стране в середине и во второй половине 1990-х годов.", Price = 2000, ReleaseYear = new DateTime(1997,03,10), Title = "Ураган", Availability = true},
+           new Group {Name = "Кукрыниксы", FoundingDate = new DateTime(1997, 07,17), HistoryOfGroup = "российская рок-группа, основанная Алексеем Горшенёвым («Ягодой»), младшим братом Михаила Горшенёва («Горшка»), лидера группы «Король и Шут».",
+                        Title = "Шаман", Price = 1750, Availability = true, ReleaseYear = new DateTime(2006,10,07)},
+           new Artist {Name = "Егор Летов", YearOfBirth = new DateTime(1964,09,10), Biography = "Игорь Фёдорович Летов (10 сентября 1964, Омск, РСФСР, СССР — 19 февраля 2008, Омск, Россия), более известный как Его́р Ле́тов, — советский и российский музыкант, певец, звукорежиссёр, художник-оформитель, коллажист и поэт." +
+               " Основатель, лидер и единственный постоянный участник группы «Гражданская оборона», также известен по музыкальным проектам «Егор и Оп********е» =), «Коммунизм» и др.", Title = "Невыносимая лёгкость бытия", ReleaseYear = new DateTime(1997,05,06), Price = 1800, Availability = true},
+           new Group {Name = "Анимация", FoundingDate = new DateTime(2000, 06,16), HistoryOfGroup= "российская рок-группа, образованная в 2000 году в городе Чистополь (Татарстан). Лидер — Константин Кулясов.", Title = "ВО", Price = 1650, ReleaseYear = new DateTime(2019,07,01), Availability = true},
+           new Artist { Name = "Дэвид Боуи", Biography = " британский рок-музыкант, певец и автор песен, а также продюсер, звукорежиссёр, художник и актёр. " +
+               "На протяжении пятидесяти лет часто переосмыслял свою карьеру, успешно сочетая творчество с актуальными музыкальными направлениями, благодаря чему получил прозвище «хамелеон рок-музыки»." +
+               " При этом ему удавалось сохранять собственный узнаваемый стиль, за счёт характерной манеры исполнения и интеллектуальной глубины созданных им работ. Боуи считается новатором, в частности, благодаря своим музыкальным экспериментам в конце 1970-х.", YearOfBirth = new DateTime(1947,01,08),
+                    Title = "Black Tie White Noise", Availability = false, Price = 1444, ReleaseYear = new DateTime(1993,04,05)},
+           new Artist {Name ="Игги Поп", YearOfBirth = new DateTime(1947,04,21), Biography = "Джеймс Нью́эл О́стерберг-младший (англ. James Newell Osterberg, Jr.; 21 апреля 1947 года, Маскигон  (англ.)рус., Мичиган, США)," +
+               " более известный как И́гги Поп (Iggy Pop) — американский рок-вокалист, один из зачинателей и гуру альтернативного рока. За вклад в развитие рок-альтернативы его величают «крёстным отцом» (иногда «дедушкой») панк-рока и гранжа." +
+               " В 2009 году британский журнал Classic Rock удостоил его звания «Живая легенда».", Title ="Brick by Brick", ReleaseYear = new DateTime(1990, 02, 15), Price = 2000, Availability = false},
+           new Artist{Name = "Оззи Осборн", YearOfBirth = new DateTime(1948,12,03), Biography = " британский рок-певец, музыкант, один из основателей и участник группы Black Sabbath, оказавшей значительное влияние на появление таких музыкальных стилей, как хард-рок и хеви-метал. Успешность его карьеры и популярность принесли ему неофициальный титул «Крёстный отец хэви-метала».", Availability = false,
+                    Title = "Down to Earth", Price = 1800, ReleaseYear = new DateTime(2001,10,06)}
+        };
+    }
+    
+}
